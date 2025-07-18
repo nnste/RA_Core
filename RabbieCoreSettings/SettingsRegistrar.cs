deca22c88ec6f46de4895e50276b04e77a13d766
조금더하이얀 L <lapisraba2@naver.com>
2025-07-19 01:24:35 +0900

    dll 업데이트

--- RabbieCoreSettings/SettingsRegistrar.cs
+++ RabbieCoreSettings/SettingsRegistrar.cs
@@ -50,12 +50,24 @@ namespace RabbieCoreSettings
             {
                 Key = "SleepfaceInfo",                
                 Label = "<b>슬립 페이스 적용</b>",       
-                Description = "이 기능은 AhnDemi님의 CloseYourEyesWhenSleeping 방식을 기반으로 표정을 추가하며,\n" +
+                Description = "이 패치는 AhnDemi님의 CloseYourEyesWhenSleeping 방식을 기반으로 표정을 추가하며,\n" +
                               "dll로 HAR의 바디애드온 conditions에 Sleeping을 추가하여 자는 상태를 구분합니다.\n" +
                               "타종족도 Sleeping을 사용할 수 있으며 xml편집으로 더 많은 얼굴을 추가할 수 있습니다.",
                 Type = SettingType.Description,          
                 Section = ParentSection.Core,              
                 Category = SettingCategory.Always            
+            }, null);
+
+            Registry.RegisterItem(new SettingDefinition
+            {
+                Key = "RabbieMaskInfo",
+                Label = "<b>래비 마스크 적용</b>",
+                Description = "이 패치는 HAR 기능을 사용해 래비가 마스크를 사용시 전용 텍스처를 적용합니다.\n" +
+                              "HAR 기능의 전용텍스쳐 기능을 활용하는 패치로 쉽게 텍스처 수정이 가능하며,\n" +
+                              "xml을 참고하여 다른 의복도 추가할 수 있습니다.",
+                Type = SettingType.Description,
+                Section = ParentSection.Core,
+                Category = SettingCategory.Always
             }, null);
         }
     }
